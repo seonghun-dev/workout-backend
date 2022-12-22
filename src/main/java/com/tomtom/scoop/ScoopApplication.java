3417fa9e7274a68e141590e0bfbba6d812587560
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-12-22 14:24:56 +0900

    Fix : remove spring security temporarily

--- src/main/java/com/tomtom/scoop/ScoopApplication.java
+++ src/main/java/com/tomtom/scoop/ScoopApplication.java
@@ -2,8 +2,10 @@ package com.tomtom.scoop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+
 public class ScoopApplication {
 
 	public static void main(String[] args) {
