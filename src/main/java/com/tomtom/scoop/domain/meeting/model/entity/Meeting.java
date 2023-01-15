fe5c4c7a86d01382360398563be3279267f0e772
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-01-15 22:11:54 +0900

    Feat : add geometry type to meeting

--- src/main/java/com/tomtom/scoop/domain/meeting/model/entity/Meeting.java
+++ src/main/java/com/tomtom/scoop/domain/meeting/model/entity/Meeting.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.geo.Point;
+import org.locationtech.jts.geom.Point;
 
 import java.time.LocalDateTime;
 
