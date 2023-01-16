b4a304bead5399e3b28966df126c2197a81b9d3f
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-01-16 23:41:36 +0900

    Feat : add Review Entity

---
+++ src/main/java/com/tomtom/scoop/domain/review/model/entity/Review.java
@@ -0,0 +1,48 @@
+package com.tomtom.scoop.domain.review.model.entity;
+
+import com.tomtom.scoop.domain.common.BaseTimeEntity;
+import com.tomtom.scoop.domain.meeting.model.entity.Meeting;
+import com.tomtom.scoop.domain.user.model.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Review extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User receiverUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User reviewerUser;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(nullable = false)
+    private Boolean isReviewerHidden;
+
+    @Column(nullable = false)
+    private Boolean isReceiverHidden;
+
+    @Column(nullable = false)
+    private String comment;
+
+}
+
+
+
+}
