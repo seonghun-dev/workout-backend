2876ab966765812bb4077613f048b6ec72300803
jinmin111 <ramyeon00@gmail.com>
2023-01-16 15:30:57 +0900

    Feat : Add User Entity

---
+++ src/main/java/com/tomtom/scoop/domain/user/model/entity/Gender.java
@@ -0,0 +1,6 @@
+package com.tomtom.scoop.domain.user.model.entity;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
---
+++ src/main/java/com/tomtom/scoop/domain/user/model/entity/User.java
@@ -0,0 +1,44 @@
+package com.tomtom.scoop.domain.user.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String name;
+
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String phone;
+
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String nickname;
+
+    private Float rating;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String deviceToken;
+
+    @Column(columnDefinition = "TINYINT")
+    private boolean isDeleted;
+
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String profileImg;
+
+}
