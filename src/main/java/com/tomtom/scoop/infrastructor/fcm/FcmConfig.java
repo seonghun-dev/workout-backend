0cf0b5550a3ab9927be8ea8ae6bf37143fe22356
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-01-17 01:00:08 +0900

    Feat : add Fcm Setting

---
+++ src/main/java/com/tomtom/scoop/infrastructor/fcm/FcmConfig.java
@@ -0,0 +1,38 @@
+package com.tomtom.scoop.infrastructor.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class FcmConfig {
+
+    @Value("${fcm.value}")
+    private String FCM_PRIVATE_KEY_PATH;
+
+    @PostConstruct
+    private void initialize() {
+        try {
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseOptions options =
+                        FirebaseOptions.builder()
+                                .setCredentials(
+                                        GoogleCredentials.fromStream(
+                                                new ClassPathResource(FCM_PRIVATE_KEY_PATH).getInputStream()))
+                                .build();
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Firebase initialization failed");
+        }
+    }
+}
