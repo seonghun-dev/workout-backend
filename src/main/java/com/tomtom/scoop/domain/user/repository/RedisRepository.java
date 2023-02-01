a2002cb182efe244f6aee4fbef4a7e4a25b5a033
jinmin111 <ramyeon00@gmail.com>
2023-02-02 01:39:29 +0900

    Feat : add Redis Repository and refresh toke dto

---
+++ src/main/java/com/tomtom/scoop/domain/user/repository/RedisRepository.java
@@ -0,0 +1,10 @@
+package com.tomtom.scoop.domain.user.repository;
+
+import com.tomtom.scoop.global.common.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RedisRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByKey(String key);
+}
