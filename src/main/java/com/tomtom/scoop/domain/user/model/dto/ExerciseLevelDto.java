19fcaa89997f4f4a91499eebfe8e52b02b93a232
jinmin111 <ramyeon00@gmail.com>
2023-02-09 01:16:36 +0900

    Feat : add ExerciseLevelDto

---
+++ src/main/java/com/tomtom/scoop/domain/user/model/dto/ExerciseLevelDto.java
@@ -0,0 +1,17 @@
+package com.tomtom.scoop.domain.user.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExerciseLevelDto {
+
+    private String exerciseName;
+    private String level;
+
+
+}
