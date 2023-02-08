85162b1a3e836e65a069a98a4c7de7777fd24878
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-02-08 13:29:48 +0700

    Feat : add QuitMeetingEventHandler

---
+++ src/main/java/com/tomtom/scoop/domain/notification/event/QuitMeetingEvent.java
@@ -0,0 +1,20 @@
+package com.tomtom.scoop.domain.notification.event;
+
+import com.tomtom.scoop.domain.meeting.model.entity.Meeting;
+import com.tomtom.scoop.domain.user.model.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class QuitMeetingEvent {
+
+    private Meeting meeting;
+
+    private User owner;
+
+    private User quitUser;
+
+}
---
+++ src/main/java/com/tomtom/scoop/domain/notification/handler/QuitMeetingEventHandler.java
@@ -0,0 +1,44 @@
+package com.tomtom.scoop.domain.notification.handler;
+
+import com.tomtom.scoop.domain.notification.constants.NotificationPageConstants;
+import com.tomtom.scoop.domain.notification.event.QuitMeetingEvent;
+import com.tomtom.scoop.domain.notification.model.Notification;
+import com.tomtom.scoop.domain.notification.model.NotificationAction;
+import com.tomtom.scoop.domain.notification.repository.NotificationActionRepository;
+import com.tomtom.scoop.domain.notification.repository.NotificationRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class QuitMeetingEventHandler {
+
+    private final NotificationActionRepository NotificationActionRepository;
+    private final NotificationRepository NotificationRepository;
+
+    @EventListener
+    public void sendNotification(QuitMeetingEvent event) {
+        var meetingTitle = event.getMeeting().getTitle();
+        var joinedUserName = event.getQuitUser().getName();
+
+        NotificationAction notificationAction = NotificationAction.builder()
+                .page(NotificationPageConstants.MEETINGSETTING)
+                .contentId(event.getMeeting().getId())
+                .build();
+
+        Notification notification = Notification.builder()
+                .user(event.getOwner())
+                .title(meetingTitle + " - " + "Joining")
+                .content(joinedUserName + " has requested to quit the meeting " + meetingTitle)
+                .isDeleted(false)
+                .isRead(false)
+                .notificationAction(notificationAction)
+                .build();
+
+        NotificationActionRepository.save(notificationAction);
+        NotificationRepository.save(notification);
+
+        System.out.printf("Notify : user %s has requested to quit the meeting %s%n", joinedUserName, meetingTitle);
+    }
+}
