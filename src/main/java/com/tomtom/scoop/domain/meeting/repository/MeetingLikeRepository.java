4d57697b38b718f6fc711636065b40a803532d89
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-02-08 10:38:56 +0700

    Feat : add meeting like builder and repository

--- src/main/java/com/tomtom/scoop/domain/meeting/repository/MeetingLikeRepository.java
+++ src/main/java/com/tomtom/scoop/domain/meeting/repository/MeetingLikeRepository.java
@@ -1,7 +1,12 @@
 package com.tomtom.scoop.domain.meeting.repository;
 
+import com.tomtom.scoop.domain.meeting.model.entity.Meeting;
 import com.tomtom.scoop.domain.meeting.model.entity.MeetingLike;
+import com.tomtom.scoop.domain.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MeetingLikeRepository extends JpaRepository<MeetingLike, Long> {
+    Optional<MeetingLike> findByMeetingAndUser(Meeting meeting, User user);
 }
