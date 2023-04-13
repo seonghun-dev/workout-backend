3458b30e99a303fba18812e49cd7a40d982c1b29
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-04-14 04:32:26 +0900

    Replace: remove unnecessary notice

--- src/main/java/com/tomtom/scoop/domain/notice/repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tomtom.scoop.domain.notice.repository;
-
-import com.tomtom.scoop.domain.notice.model.entity.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
