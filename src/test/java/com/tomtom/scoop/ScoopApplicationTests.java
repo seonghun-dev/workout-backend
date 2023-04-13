1b077a719aeb5e250286a9183a5d5b4203f53da0
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-04-13 21:47:47 +0900

    Fix: add gradle profile and remove spring test

--- src/test/java/com/tomtom/scoop/ScoopApplicationTests.java
+++ src/test/java/com/tomtom/scoop/ScoopApplicationTests.java
@@ -1,15 +1,12 @@
 package com.tomtom.scoop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-@SpringBootTest
+
 class ScoopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
