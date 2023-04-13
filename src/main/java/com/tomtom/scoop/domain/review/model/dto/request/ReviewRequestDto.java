0fa66862ead09858a8a490a57a5c208729142acb
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-04-13 20:26:31 +0900

    Test: add review test file (#3)
    * Test: add Review service test file
    
    * Test: add Review create test - success

--- src/main/java/com/tomtom/scoop/domain/review/model/dto/request/ReviewRequestDto.java
+++ src/main/java/com/tomtom/scoop/domain/review/model/dto/request/ReviewRequestDto.java
@@ -1,12 +1,14 @@
 package com.tomtom.scoop.domain.review.model.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ReviewRequestDto {
     private Long meetingId;
 
