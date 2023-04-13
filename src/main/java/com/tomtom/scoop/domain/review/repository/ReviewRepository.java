3ddb5e24db09347cc126026efa1a4c7b56564115
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-04-14 01:30:10 +0900

    Feat : add ReviewRepository find

--- src/main/java/com/tomtom/scoop/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/tomtom/scoop/domain/review/repository/ReviewRepository.java
@@ -6,6 +6,7 @@ import com.tomtom.scoop.domain.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByReceiver(User receiver);
@@ -13,4 +14,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByReviewer(User reviewer);
 
     List<Review> findByMeeting(Meeting meeting);
+
+    Optional<Review> findByMeetingAndReceiverAndReviewer(Meeting meeting, User Receiver, User reviewer);
 }
