5c17cd63cba66f8f8eb1c70fc5df7987e6759e73
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-04-15 21:21:04 +0900

    Feat : change userId to ReqUser

--- src/main/java/com/tomtom/scoop/domain/notification/controller/NotificationController.java
+++ src/main/java/com/tomtom/scoop/domain/notification/controller/NotificationController.java
@@ -2,6 +2,8 @@ package com.tomtom.scoop.domain.notification.controller;
 
 import com.tomtom.scoop.domain.notification.model.dto.NotificationListResponseDto;
 import com.tomtom.scoop.domain.notification.service.NotificationService;
+import com.tomtom.scoop.domain.user.model.entity.User;
+import com.tomtom.scoop.global.annotation.ReqUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
@@ -17,21 +19,18 @@ public class NotificationController {
 
     @GetMapping
     @ResponseBody
-    public List<NotificationListResponseDto> findAllNotifications(@RequestParam("userId") Long userId,
-                                                                  Pageable pageable) {
-        return notificationService.findAllNotifications(userId, pageable);
+    public List<NotificationListResponseDto> findAllNotifications(@ReqUser() User user, Pageable pageable) {
+        return notificationService.findAllNotifications(user, pageable);
     }
 
     @PostMapping("/{notificationId}/read")
-    public void markNotificationsAsRead(@RequestParam("userId") Long userId,
-                                        @PathVariable("notificationId") Long notificationId) {
-        notificationService.markNotificationsAsRead(userId, notificationId);
+    public void markNotificationsAsRead(@ReqUser() User user, @PathVariable("notificationId") Long notificationId) {
+        notificationService.markNotificationsAsRead(user, notificationId);
     }
 
     @DeleteMapping("/{notificationId}")
-    public void deleteNotification(@RequestParam("userId") Long userId,
-                                   @PathVariable("notificationId") Long notificationId) {
-        notificationService.deleteNotification(userId, notificationId);
+    public void deleteNotification(@ReqUser() User user, @PathVariable("notificationId") Long notificationId) {
+        notificationService.deleteNotification(user, notificationId);
     }
 
 
