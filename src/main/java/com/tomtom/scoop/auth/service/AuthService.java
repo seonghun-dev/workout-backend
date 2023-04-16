6b95a0cc2fa59c0837b756c2ef44e1276341b06d
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-04-16 15:53:49 +0900

    Refactor: auth service refactoring

--- src/main/java/com/tomtom/scoop/auth/service/AuthService.java
+++ src/main/java/com/tomtom/scoop/auth/service/AuthService.java
@@ -1,22 +1,17 @@
 package com.tomtom.scoop.auth.service;
 
 import com.tomtom.scoop.auth.model.TokenDto;
-import com.tomtom.scoop.auth.model.dao.LogoutAccessToken;
 import com.tomtom.scoop.auth.model.dao.RefreshToken;
-import com.tomtom.scoop.auth.repository.LogoutAccessTokenRepository;
 import com.tomtom.scoop.auth.repository.RefreshTokenRepository;
 import com.tomtom.scoop.auth.util.JwtTokenUtil;
-import com.tomtom.scoop.domain.user.model.entity.User;
 import com.tomtom.scoop.domain.user.repository.UserRepository;
 import com.tomtom.scoop.global.exception.BusinessException;
 import com.tomtom.scoop.global.exception.ErrorCode;
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,43 +21,25 @@ public class AuthService {
 
     private final RefreshTokenRepository refreshTokenRepository;
 
-    private final LogoutAccessTokenRepository logoutAccessTokenRepository;
-
     private final UserRepository userRepository;
 
     public TokenDto reissue(HttpServletRequest request) {
-        try {
-            String token = jwtTokenUtil.resolveToken(request);
-            String oauthId = jwtTokenUtil.getOauthId(token);
+        String token = jwtTokenUtil.resolveToken(request);
+        String oauthId = jwtTokenUtil.getOauthId(token);
 
-            RefreshToken refreshToken = refreshTokenRepository.findById(oauthId).orElseThrow(() -> new BusinessException(ErrorCode.UNAUTHORIZED));
+        RefreshToken refreshToken = refreshTokenRepository.findById(oauthId).orElseThrow(() -> new BusinessException(ErrorCode.JWT_REFRESH_TOKEN_EXPIRED));
 
-            boolean isTokenValid = jwtTokenUtil.validate(refreshToken.getValue(), oauthId);
-
-            if (isTokenValid) {
-
-                Optional<User> findUser = userRepository.findByOauthId(oauthId);
-
-                if (findUser.isPresent()) {
-                    String newAccessToken = jwtTokenUtil.generateAccessToken(oauthId);
-                    String newRefreshToken = jwtTokenUtil.generateRefreshToken(oauthId);
-
-
-                    refreshTokenRepository.save(new RefreshToken(oauthId, newRefreshToken, Duration.ofDays(14).toMillis()));
-
-                    return new TokenDto(newAccessToken, newRefreshToken);
-                }
-            }
-        } catch (ExpiredJwtException e) {
-            throw new BusinessException(ErrorCode.JWT_REFRESH_TOKEN_EXPIRED);
+        if (!jwtTokenUtil.validate(refreshToken.getValue(), oauthId)) {
+            throw new BusinessException(ErrorCode.INVALID_TOKEN);
         }
-        return null;
-    }
 
-    public void logout(TokenDto tokenDto, String oauthId) {
-        String accessToken = tokenDto.getAccessToken();
-        long remainTime = jwtTokenUtil.getRemainTime(accessToken);
-        refreshTokenRepository.deleteById(oauthId);
-        logoutAccessTokenRepository.save(LogoutAccessToken.builder().key(oauthId).value(accessToken).expiredTime(remainTime).build());
+        userRepository.findByOauthId(oauthId).orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        String newAccessToken = jwtTokenUtil.generateAccessToken(oauthId);
+        String newRefreshToken = jwtTokenUtil.generateRefreshToken(oauthId);
+
+        refreshTokenRepository.save(new RefreshToken(oauthId, newRefreshToken, Duration.ofDays(14).toMillis()));
+
+        return new TokenDto(newAccessToken, newRefreshToken);
     }
 }
