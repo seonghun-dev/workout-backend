29e3b7fd4afc665df831593f0669f408c83ded18
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-04-25 00:07:09 +0900

    Feat : add prometheus and grafana (#17)
    * Feat : add prometheus
    
    * Feat : add grafana

--- src/main/java/com/tomtom/scoop/domain/user/repository/UserLocationRepository.java
+++ src/main/java/com/tomtom/scoop/domain/user/repository/UserLocationRepository.java
@@ -1,11 +1,7 @@
 package com.tomtom.scoop.domain.user.repository;
 
-import com.tomtom.scoop.domain.user.model.entity.User;
 import com.tomtom.scoop.domain.user.model.entity.UserLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserLocationRepository extends JpaRepository<UserLocation, Long> {
-    Optional<UserLocation> findByUser(User user);
 }
